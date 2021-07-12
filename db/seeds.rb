322a921b24124d976d911f8f28c85201f6420362
joshua braclett <joshua.brackett@hotmail.ca>
2021-07-12 19:55:20 -0400

    added controller and view for flights to show all flights

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Airport.create(code: 'YYZ')
+# Airport.create(code: 'JAL')
+# Airport.create(code: 'SYD')
+# Airport.create(code: 'YVR')
+# Airport.create(code: 'YEG')
+# Airport.create(code: 'CAL')
+# Airport.create(code: 'KAL')
+
+#Flight.create(departure_airport_id: 1, destination_airport_id: 2, date: "20-1-2012".to_datetime, flight_duration: 60)
+# Flight.create(departure_airport_id: 2, destination_airport_id: 3, date: "20-26-2017".to_datetime, flight_duration: 50)
+# Flight.create(departure_airport_id: 3, destination_airport_id: 4, date: "10-12-2020".to_datetime, flight_duration: 69)
+# Flight.create(departure_airport_id: 4, destination_airport_id: 5, date: "08-25-2008".to_datetime, flight_duration: 120)
+# Flight.create(departure_airport_id: 5, destination_airport_id: 6, date: "30-19-2016".to_datetime, flight_duration: 20)
+# Flight.create(departure_airport_id: 6, destination_airport_id: 7, date: "24-10-2011".to_datetime, flight_duration: 300)
